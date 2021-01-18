cfc92aa42ad88e34b4e3a3789592ae6ba96bb95d
LAPTOP-F757HK4E\Sattapong <sattapong.torton@gmail.com>
2021-01-18 14:09:20 +0700

    01-18-2021

--- resources/views/fontend/index.blade.php
+++ resources/views/fontend/index.blade.php
@@ -23,8 +23,7 @@
 									<a target="balck" href="{{ $value->link }}" class="viewbanner">Read more <svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-arrow-right" fill="#fff" xmlns="http://www.w3.org/2000/svg">
 											<path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
 										</svg></a>
-
-								</div>
+									</div>
 								<figure><img src="{{ asset('public/assets/images/home_images/'.$value->images_pc.'') }}"></figure>
 							</div>
 						@endforeach
@@ -172,8 +171,6 @@
 											<h5>Salt-Spray <br>
 												Testing Service
 											</h5>
-
-
 										</span> </div>
 									<div class='img img4'></div>
 								</a>
